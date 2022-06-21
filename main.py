617d5e9ff1a1f85bb3fa932fa1b8d52d547f3830
Louis Laverne <louis.laverne@epita.fr>
2022-06-21 18:43:54 +0200

    Added register, activity trigger, time system and basic functions. Procfile added.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # bot.py
 # region Library
 import os
-
+import system_functions
 import discord
 from dotenv import load_dotenv
 import json
@@ -17,7 +17,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected !')
-    await client.change_presence(activity = discord.Game(name="You are a fucking nerd"))
+    await client.change_presence(activity = discord.Game(name="$start | You are a fucking nerd"))
 
 @client.event
 async def on_message(message):
@@ -25,24 +25,36 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.lower().startwith('$start'):
-        players = open('players.json', 'r+', encoding="utf-8")
-        json.load(players)
-        players_list = players['players']
+    if message.content.lower().startswith('$start'):
+        if system_functions.is_registered(message.author):
+            await message.channel.send('{}'.format(message.author.mention) + ", your League of Legends games are already looked up !")
+        else:
+            system_functions.watch_games(message.author)
+            await message.channel.send('{}'.format(message.author.mention) + ", your games are now registered !")
+
 
     if message.content.lower().startswith('$ut'):
-        players = open('players.json', 'r+', encoding="utf-8")
-        json.load(players)
-        players_list = players['players']
-        if 429991199865307138 in players_list:
-            print("True")
-        else:
-            print("False")
+        members = client.get_all_members()
+        for mb in members:
+            if mb.name == "Gx" and mb.activity.name == "League of Legends":
+                await message.channel.send("OMG Gx joue a LoL")
+
 
 
 @client.event
 async def on_member_update(before, after):
-    return
+    if before.activity == after.activity:
+        return
 
+    if not system_functions.is_registered(before):
+        return
+
+    if (not system_functions.is_playing_lol(before)) and system_functions.is_playing_lol(after):
+        system_functions.start_time(after)
+        return
+
+    if system_functions.is_playing_lol(before) and not system_functions.is_playing_lol(after):
+        system_functions.add_global_time(after)
+        return
 
 client.run(TOKEN)
