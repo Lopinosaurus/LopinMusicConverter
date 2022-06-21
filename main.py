ed5360717ab256607b7a909f9c496d01142727db
Louis Laverne <louis.laverne@epita.fr>
2022-06-21 16:18:21 +0200

    Indev member update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ client = discord.Client(intents=intents)
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected !')
-    await client.change_presence(activity = discord.Game(name="You're a fucking nerd"))
+    await client.change_presence(activity = discord.Game(name="You are a fucking nerd"))
 
 @client.event
 async def on_message(message):
@@ -25,6 +25,24 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.lower().startwith('$start'):
+        players = open('players.json', 'r+', encoding="utf-8")
+        json.load(players)
+        players_list = players['players']
+
+    if message.content.lower().startswith('$ut'):
+        players = open('players.json', 'r+', encoding="utf-8")
+        json.load(players)
+        players_list = players['players']
+        if 429991199865307138 in players_list:
+            print("True")
+        else:
+            print("False")
+
+
 @client.event
 async def on_member_update(before, after):
-    return 
+    return
+
+
+client.run(TOKEN)
